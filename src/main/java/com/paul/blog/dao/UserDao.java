d14a1411ae96908f21c6c68e82ba7fd5ce4e9e14
xiaotiantian <wangcantian@163.com>
2018-05-24 18:01:17 +0800

    1805241801

---
+++ src/main/java/com/paul/blog/dao/UserDao.java
@@ -0,0 +1,11 @@
+package com.paul.blog.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author xiaotiantian
+ * @create 2018-05-24 18:00
+ */
+@Mapper
+public class UserDao {
+}
---
+++ src/main/java/com/paul/blog/model/User.java
@@ -0,0 +1,74 @@
+package com.paul.blog.model;
+
+/**
+ * @author xiaotiantian
+ * @create 2018-05-24 17:55
+ */
+public class User {
+
+    private int id;
+    private String name;
+    private String password;
+    private String salt;
+    private String headUrl;
+    private String role;
+
+    public User() {
+    }
+
+    public User(String name){
+        this.name = name;
+        this.password = "";
+        this.salt = "";
+        this.headUrl = "";
+        this.role = "user";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getHeadUrl() {
+        return headUrl;
+    }
+
+    public void setHeadUrl(String headUrl) {
+        this.headUrl = headUrl;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
